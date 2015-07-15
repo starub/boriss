c2e045202bfe30f9d25411db8484e44a475e1231
Stanislavs Rubens <stanislavs.rubens@leongaming.com>
2015-07-15 17:57:15 +0200

    removed thymeleaf dependency;

--- src/main/java/com/boriss/Application.java
+++ src/main/java/com/boriss/Application.java
@@ -1,18 +1,10 @@
-package com.boriss.application;
+package com.boriss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @SpringBootApplication
 public class Application {
-
-    @RequestMapping("/")
-    String index() {
-        return "index";
-    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
---
+++ src/main/java/com/boriss/presentation/IndexController.java
@@ -0,0 +1,14 @@
+package com.boriss.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    String index() {
+        return "index.html";
+    }
+
+}
