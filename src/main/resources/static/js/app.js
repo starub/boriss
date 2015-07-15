4767387afab6fdbbd86d917bbbaefb8a554bed63
Stanislavs Rubens <stanislavs.rubens@leongaming.com>
2015-07-15 18:47:19 +0200

    angular material try-outs;

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,10 +2,27 @@
 
 (function() {
 
-    angular.module('IndexApplication', [])
+    angular.module('IndexApplication', [ 'ngMaterial' ])
 
-    .controller('IndexController', function($scope) {
-	$scope.welcomeMessage = 'Hello world!';
+    .controller('IndexController', function($scope, $mdDialog) {
+
+	$scope.login = function(event) {
+	    $mdDialog.show(
+		      $mdDialog.alert()
+		        .parent(angular.element(document.body))
+		        .title('This is an alert title')
+		        .content('You can specify some description text in here.')
+		        .ariaLabel('Alert Dialog Demo')
+		        .ok('Got it!')
+		        .targetEvent(event)
+		    );
+	};
+
+	$scope.register = function(event) {
+	    console.log('register');
+	};
+
+	$scope.toolbarTitle = 'John Kemp';
     })
-    
+
 })();
