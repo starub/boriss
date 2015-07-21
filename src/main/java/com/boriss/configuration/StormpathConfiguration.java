b98164d9751bac4475ec589f2cbed25552b9e035
ak <arnold.krutolevich@gmail.com>
2015-07-21 21:11:50 +0300

    add initial stormpath setup

---
+++ src/main/java/com/boriss/configuration/StormpathConfiguration.java
@@ -0,0 +1,40 @@
+package com.boriss.configuration;
+
+import com.google.common.collect.Iterables;
+import com.stormpath.sdk.api.ApiKeys;
+import com.stormpath.sdk.application.Application;
+import com.stormpath.sdk.application.Applications;
+import com.stormpath.sdk.client.Client;
+import com.stormpath.sdk.client.Clients;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("stormpath")
+public class StormpathConfiguration {
+
+    private String id;
+
+    private String secret;
+
+    @Bean
+    public Application stormpathApplication() {
+        return Iterables.getOnlyElement(stormpathClient().getCurrentTenant().getApplications(
+                Applications.where(Applications.name().eqIgnoreCase("boriss"))
+        ));
+    }
+
+    @Bean
+    public Client stormpathClient() {
+        return Clients.builder().setApiKey(ApiKeys.builder().setId(id).setSecret(secret).build()).build();
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+}
