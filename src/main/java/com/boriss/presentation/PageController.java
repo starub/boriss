cf76dca3eb5e0cba240e4f0105858b6d27f7b8c6
ak <arnold.krutolevich@gmail.com>
2015-07-22 21:14:26 +0300

    change stormpath api key

--- src/main/java/com/boriss/presentation/PageController.java
+++ src/main/java/com/boriss/presentation/PageController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class PageController {
 
     @RequestMapping("/")
-    String index() {
-        return "index.html";
+    public String index() {
+        return "index";
     }
-
 }
